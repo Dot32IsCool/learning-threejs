6d1fac88e63c49efcc4b80d73bc0b617b0c97db3
Dot32 <61964090+Dot32IsCool@users.noreply.github.com>
2021-05-30 13:56:50 +0800

    rotating point light and ambient light

--- main.js
+++ main.js
@@ -15,20 +15,25 @@ renderer.render(scene, camera);
 
 const geometry = new THREE.TorusGeometry(10, 3, 16, 100);
 // const material = new THREE.MeshBasicMaterial({color: 0x0091ff, wireframe: true});
-const material = new THREE.MeshStandardMaterial({color: 0x0091ff});
+const material = new THREE.MeshStandardMaterial({color: 0xffffff, wireframe: false});
 const torus = new THREE.Mesh(geometry, material);
-
 scene.add(torus);
 
-const pointLight = new THREE.PointLight(0xffffff);
+const pointLight = new THREE.PointLight(0xffff55);
 pointLight.position.set(0, 50, 0);
-
 scene.add(pointLight)
+
+const ambientLight = new THREE.AmbientLight(0x5555ff);
+scene.add(ambientLight)
+
+let time = 0
 
 function animate() {
   requestAnimationFrame(animate);
 
   torus.rotation.y += 0.01;
+  time += 0.05;
+  pointLight.position.set(Math.cos(time)*50, Math.sin(time)*50, 0);
 
   renderer.render(scene, camera);
 }
